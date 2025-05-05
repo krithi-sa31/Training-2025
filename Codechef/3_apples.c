0a6c7001df2ba10c206dbe2caaa9e690f9dd6b14
krithi-sa31 <krithisa79@gmail.com>
2025-05-05 22:26:06 +0530

    c

---
+++ Codechef/3_apples.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int t,a,o,temp;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++)
+	{
+	    scanf("%d%d",&a,&o);
+	    while(o!=0)  //this condition is to check the gcd
+	    {
+	        temp=o;
+	        o=a%o;
+	        a=temp;
+	    }
+	    printf("%d\n",a);
+	}
+
+}
